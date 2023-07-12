213d2a47552ff1746893ae820203ffb1e93069e4
Shubham Dubey <shubhamshd99@gmail.com>
2023-07-13 00:39:41 +0530

    added validation middleware for review posting using Joi

---
+++ src/middlewares/validation.ts
@@ -0,0 +1,15 @@
+import Joi, { ValidationResult } from 'joi';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+
+export default function validate(schema: Joi.Schema): RequestHandler {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error }: ValidationResult = schema.validate(req.body);
+
+    if (error) {
+      const validationErrors = error.details.map((err) => err.message);
+      return res.status(400).json({ errors: validationErrors });
+    }
+
+    next();
+  };
+}
---
+++ src/schemas/reviewSchema.ts
@@ -0,0 +1,26 @@
+import Joi from 'joi';
+
+const reviewSchema = Joi.object({
+  dealershipName: Joi.string().required(),
+  productDetails: Joi.object({
+    make: Joi.string().required(),
+    model: Joi.string().required(),
+    variant: Joi.string().required(),
+  }),
+  rating: Joi.number().required().min(1).max(5),
+  comments: Joi.array().items(
+    Joi.object({
+      text: Joi.string().required(),
+      timestamp: Joi.date().default(Date.now),
+      user: Joi.string().required(),
+    }),
+  ),
+  media: Joi.array().items(
+    Joi.object({
+      url: Joi.string().required(),
+      type: Joi.string().required(),
+    }),
+  ),
+});
+
+export default reviewSchema;
